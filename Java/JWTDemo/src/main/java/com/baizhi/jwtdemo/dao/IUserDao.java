d7f4b41cfa433e30ec2956b91e1b9c33be23d1cb
York <York.Yanmeng@gmail.com>
2023-04-01 11:29:50 +0800

    2023/04/01

--- Java/JWTDemo/src/main/java/com/baizhi/jwtdemo/dao/IUserDao.java
+++
@@ -1,9 +0,0 @@
-package com.baizhi.jwtdemo.dao;
-
-import com.baizhi.jwtdemo.entity.UserEntity;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface IUserDao {
-    UserEntity login(UserEntity user);
-}
