9cd8011ba02a5786b32c7000bde009465ee6bc6f
York <York.Yanmeng@gmail.com>
2023-07-22 01:28:58 +0800

    2023/07/22

---
+++ Python/Learning code/ForSchool/01-05/demo01.py
@@ -0,0 +1,19 @@
+# Author - York
+# Create Time - 7/22/2023 1:11 AM
+# File Name - demo01
+# Project Name - Learning code
+
+# input
+a = input("please enter an number: ")
+
+# print
+print("a ->", a)
+
+# split
+a0, a1 = input("please enter two numbers: ").split()
+print("a0 ->", a0)
+print("a1 ->", a1)
+
+# print in the same line
+print("a0 ->", a0, end=' ')
+print("a1 ->", a1, end=' ')
