f21c3289548e671b321de725bd69d0a691ceec69
York <York.Yanmeng@gmail.com>
2023-07-22 20:21:47 +0800

    2023/07/22

--- Python/Learning code/ForSchool/01-05/demo01.py
+++ Python/Learning code/ForSchool/01-05/demo01.py
@@ -2,6 +2,7 @@
 # Create Time - 7/22/2023 1:11 AM
 # File Name - demo01
 # Project Name - Learning code
+import math
 
 # input
 a = input("please enter an number: ")
@@ -16,4 +17,14 @@ print("a1 ->", a1)
 
 # print in the same line
 print("a0 ->", a0, end=' ')
-print("a1 ->", a1, end=' ')
+print("a1 ->", a1)
+
+# calculate the triangle area
+b0, b1, b2 = input("please enter three numbers: ").split()
+b0 = int(b0)
+b1 = int(b1)
+b2 = int(b2)
+s = (b0 + b1 + b2) / 2
+
+area = math.sqrt(s * (s - b0) * (s - b1) * (s - b2))
+print("the area of this triangle is:", area)
---
+++ Python/Learning code/ForSchool/01-05/demo02.py
@@ -0,0 +1,17 @@
+# Author - York
+# Create Time - 7/22/2023 8:21 PM
+# File Name - demo02
+# Project Name - Learning code
+import turtle
+
+# turtle - draw a pentagram
+turtle.forward(200)
+turtle.right(144)
+turtle.forward(200)
+turtle.right(144)
+turtle.forward(200)
+turtle.right(144)
+turtle.forward(200)
+turtle.right(144)
+turtle.forward(200)
+turtle.right(144)
