fb3811fc2349bd04f74559b622d9d5803f5a244e
York <York.Yanmeng@gmail.com>
2023-07-23 00:37:51 +0800

    2023/07/22

--- Python/Learning code/ForSchool/02/demo05.py
+++ Python/Learning code/ForSchool/02/demo05.py
@@ -11,3 +11,14 @@ for i in arr:
 # flashback output
 for i in arr[::-1]:
     print(i)
+
+# range
+print("from 0 to 10 step 1 ->", list(range(0, 11, 1)))
+print("from 10 to 0 step -2 ->", list(range(10, -1, -2)))
+
+# sum
+print("the sum of 0 to 9 ->", sum(list(range(0, 10))))
+
+# list derivation
+print([numbers for numbers in list(range(0, 5))])
+print([numbers * 2 for numbers in list(range(0, 5))])
