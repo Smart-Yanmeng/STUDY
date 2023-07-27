96086c62e38acd06d6c9a714e1cc1b255e936683
York <York.Yanmeng@gmail.com>
2023-07-27 22:51:23 +0800

    2023/07/27

--- Python/Learning code/ForSchool/08/demo01.py
+++ Python/Learning code/ForSchool/08/demo01.py
@@ -3,4 +3,18 @@
 # File Name - demo01
 # Project Name - temp-plot.html
 
+# class
+class Student:
+    def __init__(self, name, number):
+        self.name = name
+        self.number = number
 
+    @classmethod
+    def getInfo(cls):
+        print("name ->", cls.name, "\nnumber ->", cls.number)
+
+
+stu1 = Student("York", "01")
+stu1.getInfo()
+
+print("stu1 is a student? ->", isinstance(stu1, Student))
