ca4dc541d9df8c9c95177cfd1f587e79fc8e041e
York <York.Yanmeng@gmail.com>
2023-07-30 01:30:57 +0800

    2023/07/30

---
+++ Java/CacheLinePadding/src/com/mashibing/singleton/Mgr04.java
@@ -0,0 +1,39 @@
+package src.com.mashibing.singleton;
+
+/**
+ * 上了锁的懒汉式
+ */
+public class Mgr04 {
+    private static Mgr04 INSTANCE;
+
+    private Mgr04() {
+    }
+
+    public static Mgr04 getInstance() {
+        if (INSTANCE == null) {
+            synchronized (Mgr04.class) {
+                if (INSTANCE == null) {
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    INSTANCE = new Mgr04();
+                }
+            }
+        }
+
+        return INSTANCE;
+    }
+
+    public void m() {
+        System.out.println("m");
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            new Thread(() -> System.out.println(Mgr03.getInstance().hashCode())).start();
+        }
+    }
+}
