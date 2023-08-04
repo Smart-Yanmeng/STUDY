a8cae00a0f1604affd737e8cdf378818b23776c8
York <York.Yanmeng@gmail.com>
2023-08-04 14:16:39 +0800

    2023/08/04

---
+++ Java/geek_navigation/src/main/java/com/example/geek_navigation/mapper/IAccountMapper.java
@@ -0,0 +1,10 @@
+package com.example.geek_navigation.mapper;
+
+import com.example.geek_navigation.domain.entity.BAccountEntity;
+
+import java.util.List;
+
+public interface IAccountMapper {
+
+    List<BAccountEntity> queryAccount();
+}
--- src/main/java/com/mashang/study/dao/IHomeworkDao.java
+++
@@ -1,8 +0,0 @@
-package com.mashang.study.dao;
-
-public interface IHomeworkDao {
-
-  // 判断是否提交
-  Integer judgeHomeworkSubmit();
-
-}
